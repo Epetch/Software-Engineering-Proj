46466b702100c0f5526ef60a8a37b511d1e1b2bc
edward.p <109969019+Epetch@users.noreply.github.com>
2025-02-05 16:28:17 +0000

    Initial Commit

---
+++ Household Management/Household_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Household_ManagementApp.swift
+//  Household Management
+//
+//  Created by Edward Petcher on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Household_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Household ManagementTests/Household_ManagementTests.swift
@@ -0,0 +1,17 @@
+//
+//  Household_ManagementTests.swift
+//  Household ManagementTests
+//
+//  Created by Edward Petcher on 05/02/2025.
+//
+
+import Testing
+@testable import Household_Management
+
+struct Household_ManagementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
