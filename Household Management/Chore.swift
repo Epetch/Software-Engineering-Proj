572aafee4d894507635fec80a77fd20e9b00584b
edward.p <109969019+Epetch@users.noreply.github.com>
2025-02-12 14:59:04 +0000

    saved changes

--- Household Management/Chore.swift
+++ Household Management/Chore.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class Chore {
+    var dueDate: Date
+    var description: String
+    var isComplete: Bool = false
+    var users: [User] = []
+    var setBy: User
+    
+    init(dueDate: Date, description: String, user: User, users: [User]){         self.dueDate = dueDate
+        self.description = description
+        self.setBy = user
+        self.users = users
+    }
+    
+    func markComplete(){
+        isComplete = true
+    }
+    
+    func UnmarkComplete(){
+        isComplete = false
+    }
+    
+    func isCompleted() -> Bool {
+        return isComplete
+    }
+    
+    func getDueDate() -> Date {
+        return dueDate
+    }
+    
+    func getUsers() -> [User] {
+        return users
+    }
+}
+
+
+
+class User {
+    
+}
+
