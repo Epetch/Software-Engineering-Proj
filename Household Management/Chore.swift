63bba0e0491c6a937ded37953f7ec3a9ecc1b1f1
edward.p <109969019+Epetch@users.noreply.github.com>
2025-02-17 14:35:29 +0000

    timetable class added

--- Household Management/Chore.swift
+++ Household Management/Chore.swift
@@ -11,10 +11,11 @@ class Chore {
     var dueDate: Date
     var description: String
     var isComplete: Bool = false
-    var users: [User] = []
-    var setBy: User
+    var users: [User] = [] //the list of users that can complete the chore
+    var setBy: User //the user who set the chore
     
-    init(dueDate: Date, description: String, user: User, users: [User]){         self.dueDate = dueDate
+    init(dueDate: Date, description: String, user: User, users: [User]){ //the contructor for the chore class
+        self.dueDate = dueDate
         self.description = description
         self.setBy = user
         self.users = users
@@ -39,11 +40,44 @@ class Chore {
     func getUsers() -> [User] {
         return users
     }
+    
+    func getUser() -> User {
+        return setBy
+    }
 }
 
 
-
-class User {
+class TimeTable {
+    
+    var table: [Chore] = []
+    
+    init(){
+        
+    }
+    func addChore(chore: Chore){
+        table.append(chore)
+        var isAppended: Bool = false
+        var count: Int = 0
+        while (!isAppended){
+            if table[count].getDueDate() > chore.getDueDate(){
+                table.insert(chore, at: count)
+                isAppended = true
+            }
+            if count == table.count{
+                table.append(chore)
+                isAppended = true
+            }
+        }
+    }
+    
+    func getTable() -> [Chore] {
+        return table
+    }
+    
+    func removeChore(index: Int){
+        table.remove(at: index)
+    }
     
 }
+
 
